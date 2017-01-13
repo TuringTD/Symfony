8c06aeae3d361f9b9d35e8f4837286041b35f468
liudianpeng <liudianpeng@jinangongzuo.com>
2017-01-13 15:05:21 +0800

    添加前台页面

--- src/Workshop/Bundle/FrontendBundle/Controller/DefaultController.php
+++ src/Workshop/Bundle/FrontendBundle/Controller/DefaultController.php
@@ -4,14 +4,19 @@ namespace Workshop\Bundle\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Template()
      */
     public function indexAction()
     {
-        return $this->render('WorkshopFrontendBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository('WorkshopBackendBundle:Post')
+            ->findBy(array(), array('updatedAt' => 'desc'));
+        return array('posts'=>$posts);
     }
 }
